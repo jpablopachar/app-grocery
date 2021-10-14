a2a2e2a71737f7df449cc0a0c0d48d7bcccd1687
pablo.pachar <pablo.pachar@exmerdev.com>
2021-10-14 15:51:50 -0500

    Se aplica media queries a seccion header

--- dist/index.643c084b.js
+++ dist/index.643c084b.js
@@ -1,5 +1,36 @@
 const searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = ()=>searchForm.classList.toggle('active')
-;
+const cart = document.querySelector('.shopping-cart');
+const loginForm = document.querySelector('.login-form');
+const navbar = document.querySelector('.navbar');
+document.querySelector('#search-btn').onclick = ()=>{
+    searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+document.querySelector('#cart-btn').onclick = ()=>{
+    cart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+};
+document.querySelector('#menu-btn').onclick = ()=>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+};
+window.onscroll = ()=>{
+    searchForm.classList.remove('active');
+    cart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+};
 
 //# sourceMappingURL=index.643c084b.js.map
